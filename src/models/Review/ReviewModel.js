31e67ca8ceba87a4f37e218727cd01d79678a921
Sudan Basnet <83904391+SudanBasnet@users.noreply.github.com>
2026-07-17 13:53:06 +1000

    Add: ReviewModel & ReviewSchema

---
+++ src/models/Review/ReviewModel.js
@@ -0,0 +1,19 @@
+import ReviewSchema from "./ReviewSchema.js";
+
+//!inserting new Review
+export const createReviews = (reviewobj) => {
+  return ReviewSchema(reviewobj).save();
+};
+
+// //!get Review based on role
+export const getReviews = (filter) => {
+  return ReviewSchema.find(filter);
+};
+//!update Review
+export const updateReview = (filter, obj) => {
+  return ReviewSchema.findOneAndUpdate(filter, obj, { new: true });
+};
+//!delete Review for return
+export const deleteReview = (filter) => {
+  return ReviewSchema.findOneAndDelete(filter);
+};
